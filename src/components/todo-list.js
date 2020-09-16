7ffb542aaa35e4e3ea259474ff71e68c4a73665d
Vinogradov Artem <midnight-ninja@outlook.com>
2020-09-16 14:55:33 +0300

    Arrays as properties of components

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,17 +1,19 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+  const elements = todos.map((item) => {
+    return (
+      <li class="mui-panel panel">
+        <TodoListItem {...item} />
+      </li>
+    );
+  });
   return (
     <div class="mui-row">
       <div class="mui-col-md-12">
         <ul class="mui-list--unstyled mui--appbar-height mui--appbar-line-height">
-          <li class="mui-panel panel">
-            <TodoListItem label="Drick coffee" />
-          </li>
-          <li class="mui-panel panel">
-            <TodoListItem label="Build React App" important />
-          </li>
+          {elements}
         </ul>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+  const todoData = [
+    { label: "Drink coffee", important: false },
+    { label: "Make Awesome", important: true },
+    { label: "Have a lanch", important: false },
+  ];
+
   return (
     <div class="mui-container">
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData} />
     </div>
   );
 };
