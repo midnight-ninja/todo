3e495860b4db150d88df4b0e2ccca90bb782961e
Vinogradov Artem <midnight-ninja@outlook.com>
2020-09-16 14:01:58 +0300

    React project structure (part 1)

--- src/index.js
+++ src/index.js
@@ -1,84 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const TodoList = () => {
-  const items = ["Drink coffee", "Build Awesome App"];
-
-  return (
-    <div class="mui-row">
-      <div class="mui-col-md-12">
-        <ul class="mui-list--unstyled mui--appbar-height mui--appbar-line-height">
-          <li class="mui-panel panel">
-            <div class="mui-row todo-list-item">
-              <div class="mui-col-md-6">
-                <span class="label">{items[0]}</span>
-              </div>
-              <div class="mui-col-md-6 mui--text-right">
-                <button class="mui-btn mui-btn--fab mui-btn--small mui-btn--primary">
-                  <i class="fas fa-exclamation"></i>
-                </button>
-                <button class="mui-btn mui-btn--fab mui-btn--small mui-btn--danger">
-                  <i class="far fa-trash-alt"></i>
-                </button>
-              </div>
-            </div>
-          </li>
-          <li class="mui-panel panel">
-            <div class="mui-row todo-list-item">
-              <div class="mui-col-md-6">
-                <span class="label">{items[1]}</span>
-              </div>
-              <div class="mui-col-md-6 mui--text-right">
-                <button class="mui-btn mui-btn--fab mui-btn--small mui-btn--primary">
-                  <i class="fas fa-exclamation"></i>
-                </button>
-                <button class="mui-btn mui-btn--fab mui-btn--small mui-btn--danger">
-                  <i class="far fa-trash-alt"></i>
-                </button>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const AppHeader = () => {
-  return (
-    <div class="app-header mui--appbar-height mui--appbar-line-height">
-      <div class="mui-row">
-        <div class="mui-col-md-7">
-          <h1>ToDo List</h1>
-        </div>
-        <div class="mui-col-md-5 mui--text-right">
-          <span class="mui--text-dark-secondary">2 more to do, 3 done</span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const SearchPanal = () => {
-  return (
-    <div class="mui-row">
-      <div class="mui-col-md-12">
-        <div class="mui-textfield">
-          <input type="text" placeholder="Search" />
-        </div>
-      </div>
-    </div>
-  );
-};
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
 const App = () => {
   return (
     <div class="mui-container">
-      {/* app-header */}
       <AppHeader />
-      {/* search-panal */}
-      <SearchPanal />
-      {/* doto-list */}
+      <SearchPanel />
       <TodoList />
     </div>
   );
