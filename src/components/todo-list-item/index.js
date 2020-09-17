c5d5b83b20fe9164d051a2277fcca3f04d87f0e2
Vinogradov Artem <midnight-ninja@outlook.com>
2020-09-17 14:07:06 +0300

    How setState () works

--- src/components/todo-list-item/index.js
+++ src/components/todo-list-item/index.js
@@ -4,6 +4,7 @@ import "./todo-list-item.css";
 export default class TodoListItem extends React.Component {
   state = {
     done: false,
+    important: false,
   };
 
   onLabelClick = () => {
@@ -11,27 +12,41 @@ export default class TodoListItem extends React.Component {
       done: true,
     });
   };
+  onExclamationClick = () => {
+    this.setState({
+      important: true,
+    });
+  };
 
   render() {
-    const { label, important = false } = this.props;
+    const { label } = this.props;
+    const { done, important } = this.state;
     const style = {
       color: important ? "tomato" : "black",
     };
-    const { done } = this.state;
-    let className = done ? "label" + " done" : "label";
+    let className = "todo-list-item";
+    if (done) {
+      className += " done";
+    }
+    if (important) {
+      className += " important";
+    }
 
     return (
-      <div className="mui-row todo-list-item">
+      <div className="mui-row">
         <div className="mui-col-md-10 mui-col-xs-6">
           <span className={className} style={style} onClick={this.onLabelClick}>
             {label}
           </span>
         </div>
         <div className="mui-col-md-2 mui-col-xs-6 mui--text-right">
-          <button className="mui-btn mui-btn--fab mui-btn--small mui-btn--primary">
+          <button
+            className="mui-btn mui-btn--fab mui-btn--small mui-btn--primary btn-item"
+            onClick={this.onExclamationClick}
+          >
             <i className="fas fa-exclamation"></i>
           </button>
-          <button className="mui-btn mui-btn--fab mui-btn--small mui-btn--danger">
+          <button className="mui-btn mui-btn--fab mui-btn--small mui-btn--danger btn-item">
             <i className="far fa-trash-alt"></i>
           </button>
         </div>
